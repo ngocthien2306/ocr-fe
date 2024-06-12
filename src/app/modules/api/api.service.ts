cf7db1c3f3d7b63e59d5a42c46395153d45a23ab
thien-ds <thiennguyen@i-soft.com.vn>
2024-06-12 14:58:13 +0700

    update api url

--- src/app/modules/api/api.service.ts
+++ src/app/modules/api/api.service.ts
@@ -2,21 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { MockModel, OCRResult } from '../model/dto.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private apiUrl = 'https://192.168.1.98:8080'; // Replace with your backend API URL
 
   constructor(private http: HttpClient) { }
 
   getData(): Observable<MockModel[]> {
-    return this.http.get<MockModel[]>(`${this.apiUrl}/mock-test`);
+    return this.http.get<MockModel[]>(`${environment.baseUrl}/mock-test`);
   }
 
   postData(data: any): Observable<OCRResult> {
-    return this.http.post<OCRResult>(`${this.apiUrl}/ocr/ocr-image`, data);
+    return this.http.post<OCRResult>(`${environment.baseUrl}/ocr/ocr-image`, data);
   }
 }
